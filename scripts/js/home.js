d86b86ffb0f67283f1d05905f1bdd0ffb5d48ff1
Lucas Pedroso <lucas.pedroso>
2018-10-01 11:38:35 -0300

    Inicio da criação da home

---
+++ scripts/js/home.js
@@ -0,0 +1,31 @@
+$('#projetos-3d, #contato-form').hide();
+var projetosFront = '';
+for (var projetoFront = 0; projetoFront < 10; projetoFront++) {
+    projetosFront += "<div class='projeto-container'><div class='projeto'><div class='imagem-projeto'></div></div></div>";
+}
+$('#projetos-front').append(projetosFront);
+var projetos3D = '';
+for (var projeto3D = 0; projeto3D < 10; projeto3D++) {
+    projetos3D += "<div class='projeto-container'><div class='projeto'><div class='imagem-projeto'></div></div></div>";
+}
+$('#projetos-3d').append(projetosFront);
+function selectAba(abaSelecionada) {
+    if (abaSelecionada == "front") {
+        $('.li-bar').removeClass('li-selected');
+        $('#li-front').addClass('li-selected');
+        $('#projetos-front').show();
+        $('#projetos-3d, #contato-form').hide();
+    }
+    else if (abaSelecionada == "3d") {
+        $('.li-bar').removeClass('li-selected');
+        $('#li-3d').addClass('li-selected');
+        $('#projetos-3d').show();
+        $('#projetos-front, #contato-form').hide();
+    }
+    else {
+        $('.li-bar').removeClass('li-selected');
+        $('#li-contato').addClass('li-selected');
+        $('#contato-form').show();
+        $('#projetos-front, #projetos-3d').hide();
+    }
+}
